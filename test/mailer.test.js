075710dcc93efcba79511831c5cbf1d773037a1e
FerCa <ferca23@gmail.com>
2015-04-14 23:50:59 +0200

    Extract send mail logic to a object

---
+++ test/mailer.test.js
@@ -0,0 +1,35 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var Mailer = require('../lib/mailer');
+var nodemailer = require('nodemailer');
+var settings = require('../settings');
+
+
+suite('Mailer', function(){
+    var sut;
+    var transporterSendStub;
+
+    setup(function() {
+        var transporter = nodemailer.createTransport(settings.mail.nodeMailer);
+        transporterSendStub = sinon.stub(transporter, 'sendMail');
+        sut = new Mailer(settings, transporter);
+    });
+
+    suite('send', function() {
+
+        test('Should call transporter.sendMail', function() {
+            var subject = 'New add';
+            var htmlBody = '<b>New add found</b>';
+            var mailOptions = {
+                from: settings.mail.from,
+                to: settings.mail.to,
+                subject: subject,
+                html: htmlBody
+            };
+            sut.send(subject, htmlBody);
+            assert(transporterSendStub.calledWithExactly(mailOptions, sinon.match.func));
+        });
+
+    });
+
+});
