9d9720f01e2a5c3243001052c0eb11358ca466c2
FerCa <ferca23@gmail.com>
2015-04-26 21:42:13 +0200

    Green bar for request error handling

--- test/milanuncios.test.js
+++ test/milanuncios.test.js
@@ -1,22 +1,36 @@
 var sinon = require('sinon');
 var assert = require('chai').assert;
 var Milanuncios = require('../lib/milanuncios');
-var Ad = require('../lib/ad');
+var log2out = require('log2out');
 
 suite('Milanuncios', function() {
-    var sut, requestStub;
+    var sut, requestStub, log2OutErrorStub;
+    var milanunciosSearchUrl;
 
     setup(function() {
+        milanunciosSearchUrl = 'http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1';
         requestStub = sinon.stub();
-        sut = new Milanuncios(requestStub);
+        var logger = log2out.getLogger('Milanuncios');
+        //var logger = {error: function() {}};
+        log2OutErrorStub = sinon.stub(logger, 'error');
+        sut = new Milanuncios(requestStub, logger);
     });
 
     suite('getAds', function() {
 
-        test('Should call request with correct url', function() {
-            var milanunciosSearchUrl = 'http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1';
+        function exerciceGetAds() {
             sut.getAds();
+        }
+
+        test('Should call request with correct url', function() {
+            exerciceGetAds();
             assert(requestStub.calledWithExactly(milanunciosSearchUrl, sinon.match.func));
+        });
+
+        test('If error requesting url should print error to the console', function() {
+            requestStub.callsArgWith(1, 'error');
+            exerciceGetAds();
+            sinon.assert.calledWithExactly(log2OutErrorStub, 'Error requesting: ' + milanunciosSearchUrl);
         });
 
     });
