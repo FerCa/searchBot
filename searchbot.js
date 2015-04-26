cb8ce8f8384b2fee593ef0809e734e47743ea0ca
FerCa <ferca23@gmail.com>
2015-04-26 19:42:20 +0200

    Extract milanuncios scrapping responsibility

--- searchbot.js
+++ searchbot.js
@@ -1,39 +1,21 @@
 var request = require('request');
-var cheerio = require('cheerio');
-var iconv = require('iconv-lite');
-var Ad = require('./lib/ad');
 var Mailer = require('./lib/mailer');
+var Ad = require('./lib/ad');
+var MilanunciosScraper = require('./lib/milanunciosscraper');
 var settings = require('./settings');
+
 
 request('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        //var body = iconv.decode(body, 'iso-8859-1');
-        var $ = cheerio.load(body);
 
-        $('.x1').each(function() {
-            var link = 'http://www.milanuncios.com' + $(this).find('.x7').find('a').attr('href');
-            var text = $(this).find('.x7').find('.tx').text();
-            var price = $(this).find('.x11').find('.pr').text();
-            var image = $(this).find('.ee').attr('src');
-            var title = $(this).find('.x7').find('a').text();
-            title = title.slice(0, title.lastIndexOf('particularverfotos'));
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('Link:', link);
-            console.log('Text:', text);
-            console.log('Price:', price);
-            console.log('Image:', image);
-            console.log('Title:', title);
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('-------------------------------------------------------------------------------------');
+        var milanunciosScraper = new MilanunciosScraper();
+        var ads = milanunciosScraper.extractAds(body);
 
-            var ad = new Ad(title, text, price, link, image);
+        var mailer = new Mailer(settings);
+        for(var i = 0; i < ads.length; i++) {
+            mailer.send('New add in milanuncios', ads[i].getAsHTML());
+        }
 
-            var mailer = new Mailer(settings);
-            mailer.send('New add in milanuncios', ad.getAsHTML());
-        });
     }
 });
 
---
+++ test/milanunciosscraper.test.js
@@ -0,0 +1,45 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var MilanunciosScraper = require('../lib/milanunciosscraper');
+var fs = require('fs');
+var Ad = require('../lib/ad');
+
+suite('MilanunciosScraper', function() {
+    var sut;
+    var html = fs.readFileSync('./test/milanunciosresult.html', {encoding: 'utf8'});
+
+    setup(function() {
+        sut = new MilanunciosScraper();
+    });
+
+    suite('ExtractAds', function() {
+
+        test('When called with milanuncios results html should return correct array of ads', function() {
+
+            var expectedAds = [
+                new Ad('SE VENDE GIBSON EXPLORER ZURDO/A', ' Se vende gibson explorer para zurdo/a.  ' +
+                    'Esta IMPECABLE . \nIncluyo estuche rigido. \nno me interesan cambios. \ngracias. ', '800�',
+                    'http://www.milanuncios.com/guitarras-electricas/se-vende-gibson-explorer-zurdo-a-149244887.htm',
+                    'http://91.229.239.12/fp/1492/44/guitarras-electricas/Se-vende-gibson-explorer-zurdo/a-149244887_1.jpg'),
+                new Ad('REBAJA OPORTUNIDAD GUITARRA PARA ZURDOS', ' SE VENDE Schecter  blackjack  diamond series ,  ' +
+                    'esta IMPECABLE ,  Incluyo estuche rigido ,  no me interesan cambios ,  Gracias . ', '800�',
+                    'http://www.milanuncios.com/guitarras-electricas/rebaja-oportunidad-guitarra-para-zurdos-143346084.htm',
+                    'http://91.229.239.12/fp/1433/46/guitarras-electricas/REBAJA-OPORTUNIDAD-guitarra-para-zurdos-143346084_1.jpg'),
+                new Ad('OFERTA 2 GUITARRAS ZURDAS AL PRECIO DE 1', ' Se venden 2 guitarrones zurdas al precio de 1. \ngibson explorer' +
+                    ' + schecter v = 1550�\nEstan IMPECABLES . \nincluyo fundas rigidas en ambas. \nno me interesan cambios. \ngracias. ',
+                    '1.550�', 'http://www.milanuncios.com/guitarras-electricas/oferta-2-guitarras-zurdas-al-precio-de-1-149251932.htm',
+                    'http://91.229.239.8/fp/1492/51/guitarras-electricas/Oferta-2-guitarras-zurdas-al-precio-de-1-149251932_1.jpg'),
+                new Ad('GIBSON LES PAUL STUDIO ZURDA', ' hola:  se vende guitarra gibson les paul studio zurda,  color vino,  con su ' +
+                    'estuche original;  est� pr�cticamente nueva;  poqu�simo uso;  me canso pronto de las aficiones;  precio 900 euros NO negociables;  ' +
+                    'tambi�n podr�a vender una electro-ac�stica ibanez,  tambi�n zurda,  con menos uso todav�a que la el�ctrica,  por 150 euros.  ' +
+                    'Si alguien quisiera las dos,  se las podr�a dejar en 1000 euros.  No tengo prisa ni necesidad de venderlas,  ' +
+                    'pero me da pena tenerlas sin uso y ocupando sitio. ', '900�', 'http://www.milanuncios.com/guitarras-electricas/gibson-les-paul-studio-zurda-157617914.htm',
+                    'http://91.229.239.8/fp/1576/17/guitarras-electricas/gibson-les-paul-studio-zurda-157617914_1.jpg')
+            ];
+            var actualAds = sut.extractAds(html);
+            assert.deepEqual(expectedAds, actualAds, 'Incorrect array of ads returned')
+        });
+
+    });
+
+});
