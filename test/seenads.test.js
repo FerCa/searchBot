20926705a03f75497f0bbbb77ef03f5e7f6254f6
FerCa <ferca23@gmail.com>
2015-05-12 23:47:33 +0200

    Create new class called SeenAds to control what ads are new
    This class will store all seen ads into mongo and will be able to
    distinguish a new add than one already seen.
    Green bar for finding an ad in mongo.

---
+++ test/seenads.test.js
@@ -0,0 +1,37 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var SeenAds = require('../lib/SeenAds');
+var adModel = require('../lib/mongoose/admodel');
+
+
+suite('SeenAds', function() {
+    var sut;
+    var ad, title, price, link;
+    var adModelFindOneStub;
+
+    setup(function() {
+        title = 'awesome guitar';
+        price = '600';
+        link = 'http://adds.com/aewsomeguitar';
+        ad = {title: title, price: price, link: link};
+
+        adModelFindOneStub = sinon.stub(adModel, 'findOne');
+        sut = new SeenAds(adModel);
+    });
+
+    suite('alreadySeen', function() {
+
+        test('Should call AdModel findOne with provided ad', function() {
+            sut.alreadySeen(ad, function() {});
+            sinon.assert.calledWithExactly(adModelFindOneStub, ad, sinon.match.func);
+        });
+
+        //test('Should call provided callback with false if ad is not in DB', function() {
+        //
+        //    sut.alreadySeen();
+        //
+        //});
+
+    });
+
+});
