3f941c563d2f1bdd4fef88a648bed3cd9d4aa6d9
FerCa <ferca23@gmail.com>
2015-05-13 00:09:49 +0200

    Green bar for saving new ad into DB

--- test/seenads.test.js
+++ test/seenads.test.js
@@ -7,27 +7,30 @@ var adModel = require('../lib/mongoose/admodel');
 suite('SeenAds', function() {
     var sut;
     var ad, title, price, link;
-    var adModelFindOneStub;
+    var adModelFindOneStub, adModelCreateStub;
 
     setup(function() {
         title = 'awesome guitar';
         price = '600';
         link = 'http://adds.com/aewsomeguitar';
-        ad = {title: title, price: price, link: link};
+        ad = {title: title, text: 'Selling this awesome guitar', price: price, link: link, image: 'http://ads.com/guitar.jpg'};
 
         adModelFindOneStub = sinon.stub(adModel, 'findOne');
+        adModelCreateStub = sinon.stub(adModel, 'create');
         sut = new SeenAds(adModel);
     });
 
     teardown(function() {
         adModelFindOneStub.restore();
+        adModelCreateStub.restore();
     });
 
     suite('alreadySeen', function() {
 
         test('Should call AdModel findOne with provided ad', function() {
             sut.alreadySeen(ad, function() {});
-            sinon.assert.calledWithExactly(adModelFindOneStub, ad, sinon.match.func);
+            var expectedQuery = {title: title, price: price, link: link};
+            sinon.assert.calledWithExactly(adModelFindOneStub, expectedQuery, sinon.match.func);
         });
 
         test('Should call provided callback with false if ad is not in DB', function() {
@@ -35,6 +38,13 @@ suite('SeenAds', function() {
             sut.alreadySeen(ad, callback);
             adModelFindOneStub.callArgOnWith(1, null, null);
             sinon.assert.calledWithExactly(callback, false)
+        });
+
+        test('Should call AdModel create with ad if it is not in DB', function() {
+            var callback = sinon.spy();
+            sut.alreadySeen(ad, callback);
+            adModelFindOneStub.callArgOnWith(1, null, null);
+            sinon.assert.calledWithExactly(adModelCreateStub, ad, sinon.match.func);
         });
 
     });
