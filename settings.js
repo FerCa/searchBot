2af26d039c2f47a3477d3fb412304e0124c2ff78
FerCa <ferca23@gmail.com>
2015-05-15 20:20:15 +0200

    Add expiration to ads saved to mongo

--- settings.js
+++ settings.js
@@ -10,6 +10,7 @@ var settings = {
         from: 'SearchBot <search@fercaiscoding.com>',
         to: 'ferca23@gmail.com'
     },
+    seenAdsExpiration: 15778463, // 6 months
     mongo: {
         server: 'localhost',
         connectionTimeout: 60000 // 1 minute
