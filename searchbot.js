86ed225ca317deffc240ef9aa2a85ed239f07664
FerCa <ferca23@gmail.com>
2015-05-17 00:27:40 +0200

    Generalize milanuncios class to a webpage domain object

--- searchbot.js
+++ searchbot.js
@@ -1,13 +1,14 @@
 var Mailer = require('./lib/mailer');
 var settings = require('./settings');
-var Milanuncios = require('./lib/milanuncios');
+var Webpage = require('./lib/webpage');
 var SeenAds = require('./lib/seenads');
+var MilanunciosScraper = require('./lib/milanunciosscraper');
 
 
-var milanuncios = new Milanuncios();
+var webpage = new Webpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1', new MilanunciosScraper());
 var seenAds = new SeenAds(settings.mongo);
 
-milanuncios.getAds(function (ads) {
+webpage.getAds(function (ads) {
 
     var mailer = new Mailer(settings);
 
@@ -15,7 +16,7 @@ milanuncios.getAds(function (ads) {
         var ad = ads[i];
         seenAds.alreadySeen(ad, function(seen) {
             if(!seen) {
-                mailer.send('New add in milanuncios', ad.getAsHTML());
+                mailer.send('New add', ad.getAsHTML());
             }
         });
     }
--- test/webpage.test.js
+++ test/webpage.test.js
@@ -1,22 +1,22 @@
 var sinon = require('sinon');
 var assert = require('chai').assert;
-var Milanuncios = require('../lib/milanuncios');
+var Webpage = require('../lib/webpage');
 var log2out = require('log2out');
 var MilanunciosScraper = require('../lib/milanunciosscraper');
 
-suite('Milanuncios', function() {
-    var sut, requestStub, log2OutErrorStub, milanunciosScraperExtractAdsStub;
-    var milanunciosSearchUrl;
+suite('Webpage', function() {
+    var sut, requestStub, log2OutErrorStub, scraperExtractAdsStub;
+    var searchUrl;
 
     setup(function() {
-        milanunciosSearchUrl = 'http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1';
+        searchUrl = 'http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1';
         requestStub = sinon.stub();
-        var logger = log2out.getLogger('Milanuncios');
+        var logger = log2out.getLogger('Webpage');
         log2OutErrorStub = sinon.stub(logger, 'error');
-        var milanunciosScraper = new MilanunciosScraper();
-        milanunciosScraperExtractAdsStub = sinon.stub(milanunciosScraper, 'extractAds');
+        var scraper = new MilanunciosScraper();
+        scraperExtractAdsStub = sinon.stub(scraper, 'extractAds');
 
-        sut = new Milanuncios(requestStub, logger, milanunciosScraper);
+        sut = new Webpage(searchUrl, scraper, requestStub, logger);
     });
 
     suite('getAds', function() {
@@ -33,35 +33,35 @@ suite('Milanuncios', function() {
 
         test('Should call request with correct url', function() {
             exerciceGetAds();
-            assert(requestStub.calledWithExactly(milanunciosSearchUrl, sinon.match.func));
+            assert(requestStub.calledWithExactly(searchUrl, sinon.match.func));
         });
 
         test('If error requesting url should print error to the console', function() {
             requestStub.callsArgWith(1, 'error');
             exerciceGetAds();
-            sinon.assert.calledWithExactly(log2OutErrorStub, 'Error requesting: ' + milanunciosSearchUrl);
+            sinon.assert.calledWithExactly(log2OutErrorStub, 'Error requesting: ' + searchUrl);
         });
 
         test('If http error returned when requesting url should print error to the console', function() {
             var fakeResponse = createFakeResponseWithStatusCode(500);
             requestStub.callsArgWith(1, undefined, fakeResponse);
             exerciceGetAds();
-            sinon.assert.calledWithExactly(log2OutErrorStub, 'Error requesting: ' + milanunciosSearchUrl);
+            sinon.assert.calledWithExactly(log2OutErrorStub, 'Error requesting: ' + searchUrl);
         });
 
-        test('If http response is 200 should call milanunciosScraper.extractAds with response body', function() {
+        test('If http response is 200 should call provided scraper extractAds with response body', function() {
             var fakeResponse = createFakeResponseWithStatusCode(200);
             var body = 'testHtmlBody';
             requestStub.callsArgWith(1, undefined, fakeResponse, body);
             exerciceGetAds(function(){});
-            sinon.assert.calledWithExactly(milanunciosScraperExtractAdsStub, body);
+            sinon.assert.calledWithExactly(scraperExtractAdsStub, body);
         });
 
-        test('If http response is 200 should call provided callback with the result of milanunciosScrapper.extractAds', function() {
+        test('If http response is 200 should call provided callback with the result of the provided scraper extractAds', function() {
             var fakeResponse = createFakeResponseWithStatusCode(200);
             requestStub.callsArgWith(1, undefined, fakeResponse, 'body');
             var expectedAds = 'arrayOfAds';
-            milanunciosScraperExtractAdsStub.returns(expectedAds);
+            scraperExtractAdsStub.returns(expectedAds);
             var callbackSpy = sinon.spy();
             exerciceGetAds(callbackSpy);
             callbackSpy.calledWithExactly(expectedAds);
