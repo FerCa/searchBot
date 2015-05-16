ce97a94a973888113952c39656206080ef6a2b85
FerCa <ferca23@gmail.com>
2015-05-17 00:56:03 +0200

    Extract milanuncios logic into a webpage decorator class

--- searchbot.js
+++ searchbot.js
@@ -1,11 +1,10 @@
 var Mailer = require('./lib/mailer');
 var settings = require('./settings');
-var Webpage = require('./lib/webpage');
+var MilanunciosWebpage = require('./lib/milanuncioswebpage');
 var SeenAds = require('./lib/seenads');
-var MilanunciosScraper = require('./lib/milanunciosscraper');
 
 
-var webpage = new Webpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1', new MilanunciosScraper());
+var webpage = new MilanunciosWebpage();
 var seenAds = new SeenAds(settings.mongo);
 
 webpage.getAds(function (ads) {
---
+++ test/milanuncioswebpage.test.js
@@ -0,0 +1,29 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var Webpage = require('../lib/webpage');
+var MilanunciosWebpage = require('../lib/milanuncioswebpage');
+
+suite('MilanunciosWebpage', function() {
+    var sut, webPage, webPageGetAdsStub;
+
+    setup(function() {
+        var webpage = new Webpage();
+        webPageGetAdsStub = sinon.stub(webpage, 'getAds');
+        sut = new MilanunciosWebpage(webpage);
+    });
+
+    suite('getAds', function() {
+
+        function exerciceGetAds(callback) {
+            sut.getAds(callback);
+        }
+
+        test('Should call webPage getAds with provided callback', function() {
+            var callback = function(){};
+            exerciceGetAds(callback);
+            assert(webPageGetAdsStub.calledWithExactly(callback));
+        });
+
+    });
+
+});
