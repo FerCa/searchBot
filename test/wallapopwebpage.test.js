0578e2684a7d68414804f6a2fef9bf40ce934010
FerCa <ferca23@gmail.com>
2015-05-17 12:46:01 +0200

    Create WallapopWebpage class
    There is duplication between WallapopWebpage and MilanunciosWebpage and
    it will grow when new search pages will be suported. Maybe will be
    better to solve this using inheritance instead of composition.

---
+++ test/wallapopwebpage.test.js
@@ -0,0 +1,29 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var Webpage = require('../lib/webpage');
+var WallapopWebpage = require('../lib/wallapopwebpage');
+
+suite('WallapopWebpage', function() {
+    var sut, webPage, webPageGetAdsStub;
+
+    setup(function() {
+        var webpage = new Webpage();
+        webPageGetAdsStub = sinon.stub(webpage, 'getAds');
+        sut = new WallapopWebpage('searchurl', webpage);
+    });
+
+    suite('getAds', function() {
+
+        function exerciceGetAds(callback) {
+            sut.getAds(callback);
+        }
+
+        test('Should call webPage getAds with provided callback', function() {
+            var callback = function(){};
+            exerciceGetAds(callback);
+            assert(webPageGetAdsStub.calledWithExactly(callback));
+        });
+
+    });
+
+});
