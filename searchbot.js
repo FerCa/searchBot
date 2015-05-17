6ab7caa59f06df047b7808d18a68a671e91541a8
FerCa <ferca23@gmail.com>
2015-05-17 20:22:05 +0200

    Spike solution to get notifications for multiple pages

--- searchbot.js
+++ searchbot.js
@@ -5,22 +5,24 @@ var WallapopWebpage = require('./lib/wallapopwebpage');
 var SeenAds = require('./lib/seenads');
 
 
-//var webpage = new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1');
-var webpage = new WallapopWebpage('http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056');
+var webpages = [
+    new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1'),
+    new WallapopWebpage('http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056')
+];
 var seenAds = new SeenAds(settings.mongo);
+var mailer = new Mailer(settings);
 
-webpage.getAds(function (ads) {
+for(var j = 0; j < webpages.length; j++) {
+    webpages[j].getAds(function (ads) {
+        for(var i = 0; i < ads.length; i++) {
+            var ad = ads[i];
+            seenAds.alreadySeen(ad, function(seen, providedAd) {
+                if(!seen) {
+                    mailer.send('New add', providedAd.getAsHTML());
+                }
+            });
+        }
 
-    var mailer = new Mailer(settings);
-
-    for(var i = 0; i < ads.length; i++) {
-        var ad = ads[i];
-        seenAds.alreadySeen(ad, function(seen, providedAd) {
-            if(!seen) {
-                mailer.send('New add', providedAd.getAsHTML());
-            }
-        });
-    }
-
-});
+    });
+}
 
