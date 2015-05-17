bc0fe07403384cc245faee6f5434aec012ecd70a
FerCa <ferca23@gmail.com>
2015-05-17 12:49:59 +0200

    Connect WallapopWebpage to do some manual functional test

--- searchbot.js
+++ searchbot.js
@@ -1,10 +1,12 @@
 var Mailer = require('./lib/mailer');
 var settings = require('./settings');
 var MilanunciosWebpage = require('./lib/milanuncioswebpage');
+var WallapopWebpage = require('./lib/wallapopwebpage');
 var SeenAds = require('./lib/seenads');
 
 
-var webpage = new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1');
+//var webpage = new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1');
+var webpage = new WallapopWebpage('http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056');
 var seenAds = new SeenAds(settings.mongo);
 
 webpage.getAds(function (ads) {
