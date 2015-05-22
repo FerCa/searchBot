984ed189d67a88ffda618848576508757338ec0a
FerCa <ferca23@gmail.com>
2015-05-22 20:02:21 +0200

    Connect new webpages class

--- searchbot.js
+++ searchbot.js
@@ -2,17 +2,11 @@ var settings = require('./settings');
 var MilanunciosWebpage = require('./lib/milanuncioswebpage');
 var WallapopWebpage = require('./lib/wallapopwebpage');
 var AdsChecker = require('./lib/adschecker');
+var Webpages = require('./lib/webpages');
 
-var webpages = [
-    new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1'),
-    new WallapopWebpage('http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056')
-];
+var webpages = new Webpages();
 
-var adsChecker = new AdsChecker();
+webpages.add(new MilanunciosWebpage('http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1'));
+webpages.add(new WallapopWebpage('http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056'));
 
-for(var j = 0; j < webpages.length; j++) {
-    webpages[j].getAds(function (ads) {
-        adsChecker.checkAndNotify(ads);
-    });
-}
-
+webpages.process();
