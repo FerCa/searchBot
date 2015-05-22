35fb2248c1e9c5102a652f89ee77807f1158d8d7
FerCa <ferca23@gmail.com>
2015-05-22 23:43:03 +0200

    Add some validation when reading searches config file
    If a format error is detected reading the file will try to skip it and
    log a error message.

--- searchbot.js
+++ searchbot.js
@@ -1,15 +1,28 @@
 var Webpages = require('./lib/webpages');
 var searches = require('./searches');
+var log2out = require('log2out');
+var logger = log2out.getLogger('searchBot');
 
 for(var i = 0; i < searches.length; i++) {
 
     var search = searches[i];
     var webpages = new Webpages(search.name);
 
+    if(!Array.isArray(search.where)) {
+        logger.error('Invalid where value for ' + search.name);
+        continue;
+    }
+
     for(var j = 0; j < search.where.length; j++) {
 
         var where = search.where[j];
-        var Webpage = require('./lib/webpages/' + where.page + '/' + where.page + 'webpage.js');
+        try {
+            var Webpage = require('./lib/webpages/' + where.page + '/' + where.page + 'webpage.js');
+        } catch(e) {
+            logger.error(where.page + ' webpage class not found.');
+            continue;
+        }
+
         webpages.add(new Webpage(where.searchUrl));
     }
 
