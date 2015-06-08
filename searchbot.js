47339a139bd1db9046fd7497e154c53cde7276f7
FerCa <ferca23@gmail.com>
2015-06-08 21:43:09 +0200

    Extract searches handling logic to a new domain object

--- searchbot.js
+++ searchbot.js
@@ -1,38 +1,11 @@
-var Webpages = require('./lib/webpages');
 var searches = require('./searches');
 var log2out = require('log2out');
 var logger = log2out.getLogger('searchBot');
-var Mailer = require('./lib/mailer');
-var settings = require('./settings');
+var Search = require('./lib/search');
 
-var mailer = new Mailer(settings.mail);
-
+logger.info('Starting...');
 for(var i = 0; i < searches.length; i++) {
-
-    var search = searches[i];
-    var webpages = new Webpages();
-
-    if(!Array.isArray(search.where)) {
-        logger.error('Invalid where value for ' + search.name);
-        continue;
-    }
-
-    for(var j = 0; j < search.where.length; j++) {
-
-        var where = search.where[j];
-        try {
-            var Webpage = require('./lib/webpages/' + where.page + '/' + where.page + 'webpage.js');
-        } catch(e) {
-            logger.error(where.page + ' webpage class not found.');
-            continue;
-        }
-
-        webpages.add(new Webpage(where.searchUrl));
-    }
-
-    webpages.process(function(newAdsHtml) {
-        //@TODO: Create a abstract notifier instead of using mailer.send
-        mailer.send(search.notifyTo, 'New adds for ' + search.name, newAdsHtml);
-    });
+    var search = new Search(searches[i]);
+    search.process();
 }
 
---
+++ test/search.test.js
@@ -0,0 +1,61 @@
+var sinon = require('sinon');
+var assert = require('chai').assert;
+var Search = require('../lib/search');
+var Webpages = require('../lib/webpages');
+var Mailer = require('../lib/mailer');
+var MilanunciosWebpage = require('../lib/webpages/milanuncios/milanuncioswebpage');
+var WallapopWebpage = require('../lib/webpages/wallapop/wallapopwebpage');
+
+suite('Search', function() {
+    var sut, searchPojo, name, notifyTo;
+    var mailer, mailerSendStub;
+    var webpages, webpagesAddStub, webpagesProcessStub;
+
+    setup(function() {
+        name = 'Guitars';
+        notifyTo = 'aemail@email.com';
+        searchPojo = {
+            name: name,
+            where: [
+                { page: 'milanuncios', searchUrl: 'http://www.milanuncios.com/instrumentos-musicales/guitarra-zurdo.htm?desde=400&hasta=2500&dias=1' },
+                { page: 'wallapop', searchUrl: 'http://es.wallapop.com/search?kws=guitarra+zurdo&lat=41.387245&lng=2.191056' }
+            ],
+            notifyTo: notifyTo
+        };
+        mailer = new Mailer({}, {});
+        mailerSendStub = sinon.stub(mailer, 'send');
+        webpages = new Webpages();
+        webpagesAddStub = sinon.stub(webpages, 'add');
+        webpagesProcessStub = sinon.stub(webpages, 'process');
+        sut = new Search(searchPojo, mailer, webpages);
+    });
+
+    suite('process', function() {
+
+        function exerciceProcess() {
+            return sut.process();
+        }
+
+        test('Should add to webpages a webpages implementation instance for every provided where', function() {
+            exerciceProcess();
+            assert.isTrue(webpagesAddStub.getCall(0).args[0] instanceof MilanunciosWebpage);
+            assert.isTrue(webpagesAddStub.getCall(1).args[0] instanceof WallapopWebpage);
+        });
+
+        test('Should call webpages.process', function() {
+            exerciceProcess();
+            sinon.assert.calledWithExactly(webpagesProcessStub, sinon.match.func);
+        });
+
+        test('Should call mailer.send with adsHTML returned by webpages.process', function() {
+            adsHtml = 'adsHtml';
+            exerciceProcess();
+            webpagesProcessStub.callArgWith(0, adsHtml);
+            sinon.assert.calledWithExactly(mailerSendStub, notifyTo, 'New adds for ' + name, adsHtml);
+        });
+
+
+
+    });
+
+});
